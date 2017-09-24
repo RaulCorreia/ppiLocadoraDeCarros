89861fa0cd82a35c1ecee766c06519e1376af048
RaulCorreia <new_correia@yahoo.com.br>
2017-09-24 15:18:46 -0300

    Update CarrosDao.java

--- Trabalho_ppi_1/src/ppi/agenda/dao/CarrosDao.java
+++ Trabalho_ppi_1/src/ppi/agenda/dao/CarrosDao.java
@@ -25,33 +25,10 @@ public class CarrosDao {
 		this.connection = new ConnectionFactory().getConnection();
 	}
 
+	
 	public List<Carro> obterListaCarrosDisponivel(String categoria, String dataInicial, String dataFinal) {
 
-		/*
-		 * System.out.println(categoria+" "+ dataInicial+" "+ dataFinal); List<Aluguel>
-		 * alugueis = new ArrayList<Aluguel>(); List<Carro> carros = new
-		 * ArrayList<Carro>(); List<Integer> blackList = new ArrayList<Integer>();
-		 * List<String> modelosExistentes = new ArrayList<String>();
-		 * 
-		 * // Transforma a data inicial passada para Calendar Calendar
-		 * dataInicialCalendar = Calendar.getInstance();; try { java.util.Date date1 =
-		 * new SimpleDateFormat("dd/MM/yyyy").parse(dataInicial);
-		 * dataInicialCalendar.setTime(date1); } catch (ParseException e1) {
-		 * e1.printStackTrace(); } // Transforma em Joda Date DateTime dataInicialDT =
-		 * new DateTime(dataInicialCalendar);
-		 * 
-		 * 
-		 * 
-		 * 
-		 * // Transforma a data final passada para Calendar Calendar dataFinalCalendar =
-		 * Calendar.getInstance(); try { java.util.Date date2 = new
-		 * SimpleDateFormat("dd/MM/yyyy").parse(dataFinal);
-		 * dataFinalCalendar.setTime(date2); } catch (ParseException e1) { // TODO
-		 * Auto-generated catch block e1.printStackTrace(); } // Transforma em Joda Date
-		 * DateTime dataFinalDT = new DateTime(dataFinalCalendar);
-		 * 
-		 * System.out.println("aqui2");
-		 */
+		
 		List<Carro> CarrosDisponiveis = new ArrayList<Carro>();
 
 		try {
@@ -77,10 +54,9 @@ public class CarrosDao {
 				while (rs.next()) {
 
 					Carro novoCarro = new Carro();
-					novoCarro.setId(rs.getInt("id"));
+					novoCarro.setRenavan(rs.getLong("renavan"));
 					novoCarro.setModelo(rs.getString("modelo"));
 					novoCarro.setCategoria(rs.getString("categoria"));
-					novoCarro.setRenavan(rs.getString("renavan"));
 					novoCarro.setAnoFabricacao(rs.getString("anoFabricacao"));
 					novoCarro.setTarifaDia(rs.getDouble("tarifaDia"));
 
@@ -121,232 +97,28 @@ public class CarrosDao {
 
 		}
 
-		// Obtem os dados de todos os alugueis ate o momento
-		// aluguelDao = new AlugarDao();
-		// alugueis = aluguelDao.obterListaAlugueisCompleta();
 
-		/*
-		 * if(alugueis.isEmpty()) {
-		 * 
-		 * try {
-		 * 
-		 * 
-		 * // Pega todos os carros PreparedStatement stmt = this.connection.
-		 * prepareStatement("SELECT * FROM carros where categoria like ? ;");
-		 * stmt.setString(1, categoria); ResultSet rs = stmt.executeQuery();
-		 * 
-		 * while(rs.next()) { System.out.println("rsrs");
-		 * 
-		 * Carro novoCarro = new Carro(); novoCarro.setId(rs.getInt("id"));
-		 * novoCarro.setModelo(rs.getString("modelo"));
-		 * novoCarro.setCategoria(rs.getString("categoria")); //
-		 * novoCarro.setRenavan(rs.getString("renavan")); //
-		 * novoCarro.setAnoFabricacao(rs.getString("anoFabricacao"));
-		 * //novoCarro.setTarifaDia(rs.getDouble("tarifaDia"));
-		 * 
-		 * boolean existe = false; for(int j = 0; j < carros.size(); j++) {
-		 * 
-		 * if(carros.get(j).getModelo().equals(novoCarro.getModelo())) { existe = true;
-		 * break; }
-		 * 
-		 * }
-		 * 
-		 * System.out.println(existe); if(!existe){ carros.add(novoCarro);
-		 * 
-		 * } }
-		 * 
-		 * } catch(SQLException e) {
-		 * 
-		 * }
-		 * 
-		 * }else for(int i = 0; i < alugueis.size(); i++) {
-		 * 
-		 * System.out.println("aqui2");
-		 * 
-		 * Aluguel aluguel = alugueis.get(i);
-		 * 
-		 * DateTime dataInicialAlugado = new DateTime(aluguel.getDataInicioAluguel());
-		 * DateTime dataFinalAlugado = new DateTime(aluguel.getDataFinalAluguel());
-		 * 
-		 * 
-		 * // Adiciona a blackList os que tem as datas coincidentes
-		 * if(dataInicialDT.isAfter(dataInicialAlugado) ||
-		 * dataInicialDT.isBefore(dataFinalAlugado) ||
-		 * dataFinalDT.isAfter(dataInicialAlugado) ||
-		 * dataFinalDT.isBefore(dataFinalAlugado) ||
-		 * dataInicialDT.equals(dataInicialAlugado) ||
-		 * dataFinalDT.equals(dataFinalAlugado)) {
-		 * 
-		 * blackList.add(aluguel.getIdDoCarro());
-		 * 
-		 * }
-		 * 
-		 * System.out.println("aqui1");
-		 * 
-		 * 
-		 * try {
-		 * 
-		 * 
-		 * // Pega todos os carros PreparedStatement stmt = this.connection.
-		 * prepareStatement("SELECT * FROM carros where categoria like ?;");
-		 * stmt.setString(1, categoria); ResultSet rs = stmt.executeQuery();
-		 * 
-		 * while(rs.next()) {
-		 * 
-		 * 
-		 * 
-		 * int id = rs.getInt("id"); boolean existe = false;
-		 * 
-		 * // Testa se o carro esta na blackList for(int j = 0; j < blackList.size();
-		 * j++) { if(id == blackList.get(j)) existe = true; }
-		 * 
-		 * 
-		 * // Se nao existe entao adiciona a lista if(!existe) {
-		 * 
-		 * Carro novoCarro = new Carro(); novoCarro.setId(id);
-		 * novoCarro.setModelo(rs.getString("modelo"));
-		 * novoCarro.setCategoria(rs.getString("categoria"));
-		 * novoCarro.setRenavan(rs.getString("renavan"));
-		 * novoCarro.setAnoFabricacao(rs.getString("anoFabricacao"));
-		 * novoCarro.setTarifaDia(rs.getDouble("tarifaDia"));
-		 * 
-		 * 
-		 * // Testar se e da categoria selecionado e se ja tem
-		 * 
-		 * // Se o carro obtido é da categoria que o usuario passou ele aceita
-		 * if(novoCarro.getCategoria().equals(categoria)){
-		 * 
-		 * 
-		 * // Se a lista de modelos ja tem algum carro, significa que minha lista ja tem
-		 * // Entao tem que comparar se nao sao carros iguais
-		 * if(modelosExistentes.size() > 0) {
-		 * 
-		 * boolean existeModelo = false; for(int k = 0; k < modelosExistentes.size();
-		 * k++) {
-		 * 
-		 * // Se ja existir um modelo igual vira true
-		 * if(novoCarro.getModelo().equals(modelosExistentes.get(k))) existeModelo =
-		 * true;
-		 * 
-		 * }
-		 * 
-		 * // Se Não existe o modelona lista ele adiciona a lista de carros Aptos a
-		 * alugar if(!existeModelo) { modelosExistentes.add(novoCarro.getModelo());
-		 * carros.add(novoCarro); }
-		 * 
-		 * 
-		 * 
-		 * } else { // Se minha lista de modelos existentes e igual a 0 é pq nenhum foi
-		 * adicionado // entao adiciona direto
-		 * modelosExistentes.add(novoCarro.getModelo()); carros.add(novoCarro); }
-		 * 
-		 * 
-		 * 
-		 * 
-		 * }
-		 * 
-		 * 
-		 * 
-		 * }
-		 * 
-		 * 
-		 * }
-		 * 
-		 * } catch (SQLException e) {
-		 * System.out.println("Erro ao obter carros disponiveis: " + e.getMessage()); }
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * }
-		 */
-
-		// try {
-		//
-		// PreparedStatement stmt = this.connection.prepareStatement("SELECT * FROM
-		// carros WHERE categoria like = ? AND disponivel = 1 GROUP BY modelo");
-		// stmt.setString(1, categoria);
-		// ResultSet rs = stmt.executeQuery();
-		//
-		// while(rs.next()) {
-		//
-		// Carro novoCarro = new Carro();
-		//
-		// novoCarro.setId(rs.getInt("id"));
-		// novoCarro.setModelo(rs.getString("modelo"));
-		// novoCarro.setCategoria(rs.getString("categoria"));
-		// novoCarro.setRenavan(rs.getString("renaven"));
-		// novoCarro.setAnoFabricacao(rs.getString("anoFabricacao"));
-		// novoCarro.setTarifaDia(rs.getDouble("tarifaDia"));
-		// novoCarro.setDisponivel(rs.getInt("disponivel"));
-		//
-		// if(novoCarro.getDisponivel() == 1)
-		// carros.add(novoCarro);
-		//
-		// }
-		//
-		// } catch (SQLException e) {
-		// System.out.println("Erro ao obter lista de carros disponiveis");
-		// System.out.println(e.getMessage());
-		// }*/
 
 	
 
-	// GETTERS
-
-	public List<Carro> getobterDisponivel(String categoria) {
+	public List<Carro> obterListaCarrosCompleta() {
 
 		List<Carro> carros = new ArrayList<Carro>();
 
 		try {
 
-			PreparedStatement stmt = this.connection.prepareStatement(
-					"SELECT min(id) as 'id' , modelo, categoria FROM carros where categoria like ? and disponivel=1  group  by modelo, categoria;");
-			stmt.setString(1, categoria);
-			ResultSet rs = stmt.executeQuery();
-
-			while (rs.next()) {
-
-				Carro novoCarro = new Carro();
-				novoCarro.setId(Integer.parseInt(rs.getString("id")));
-				novoCarro.setModelo(rs.getString("modelo"));
-				novoCarro.setCategoria(rs.getString("categoria"));
-
-				carros.add(novoCarro);
-
-			}
-
-		} catch (SQLException e) {
-			System.out.println("Erro ao obter lista de carros disponiveis");
-			System.out.println(e.getMessage());
-
-		}
-
-		return carros;
-
-	}
-
-	public List<Carro> getObterListaCarrosCompleta() {
-
-		List<Carro> carros = new ArrayList<Carro>();
-
-		try {
-
-			PreparedStatement stmt = this.connection.prepareStatement("SELECT * FROM carros where disponivel=1");
+			PreparedStatement stmt = this.connection.prepareStatement("SELECT * FROM carros");
 			ResultSet rs = stmt.executeQuery();
 
 			while (rs.next()) {
 
 				Carro novoCarro = new Carro();
 
-				novoCarro.setId(rs.getInt("id"));
+				novoCarro.setRenavan(rs.getLong("renavan"));
 				novoCarro.setModelo(rs.getString("modelo"));
 				novoCarro.setCategoria(rs.getString("categoria"));
-				novoCarro.setRenavan(rs.getString("renavan"));
 				novoCarro.setAnoFabricacao(rs.getString("anoFabricacao"));
 				novoCarro.setTarifaDia(rs.getDouble("tarifaDia"));
-				novoCarro.setDisponivel(rs.getInt("disponivel"));
 
 				carros.add(novoCarro);
 
@@ -361,22 +133,21 @@ public class CarrosDao {
 
 	}
 
-	public Carro obterCarro(int id) {
+	public Carro obterCarro(long renavan) {
 
 		Carro novoCarro = new Carro();
 
 		try {
 
-			PreparedStatement stmt = this.connection.prepareStatement("SELECT * FROM carros WHERE ID = ?");
-			stmt.setInt(1, id);
+			PreparedStatement stmt = this.connection.prepareStatement("SELECT * FROM carros WHERE RENAVAN = ?");
+			stmt.setLong(1, renavan);
 			ResultSet rs = stmt.executeQuery();
 
 			if (rs.next()) {
 
-				novoCarro.setId(rs.getInt("id"));
+				novoCarro.setRenavan(rs.getLong("renavan"));
 				novoCarro.setModelo(rs.getString("modelo"));
 				novoCarro.setCategoria(rs.getString("categoria"));
-				novoCarro.setRenavan(rs.getString("renavan"));
 				novoCarro.setAnoFabricacao(rs.getString("anoFabricacao"));
 				novoCarro.setTarifaDia(rs.getDouble("tarifaDia"));
 
@@ -393,17 +164,17 @@ public class CarrosDao {
 
 	// SETTERs
 
-	public void adicionaCarro(String modelo, String categoria, String renavan, String anoFabricacao, double tarifaDia,
+	public void adicionaCarro(String modelo, String categoria, Long renavan, String anoFabricacao, double tarifaDia,
 			int quantidadeMax, int quantidadeDisponivel) {
 
 		try {
 
 			PreparedStatement stmt = this.connection.prepareStatement(
-					"INSERT INTO carros(modelo, categoria, renavan, anoFabricacao,tarifaDia) VALUES (?, ?, ?, ?, ?)");
+					"INSERT INTO carros(renavan, modelo, categoria, anoFabricacao,tarifaDia) VALUES (?, ?, ?, ?, ?)");
 
-			stmt.setString(1, modelo);
-			stmt.setString(2, categoria);
-			stmt.setString(3, renavan);
+			stmt.setLong(1, renavan);
+			stmt.setString(2, modelo);
+			stmt.setString(3, categoria);
 			stmt.setString(4, anoFabricacao);
 			stmt.setDouble(5, tarifaDia);
 
@@ -416,10 +187,10 @@ public class CarrosDao {
 
 	}
 
-	public void removerCarro(int id) {
+	public void removerCarro(long renavan) {
 		try {
-			System.out.println(id);
-			PreparedStatement stmt = this.connection.prepareStatement("Delete  from carros where id=" + id + ";");
+
+			PreparedStatement stmt = this.connection.prepareStatement("Delete  from carros where renavan=" + renavan + ";");
 			stmt.execute();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -434,7 +205,7 @@ public class CarrosDao {
 		try {
 
 			PreparedStatement stmt = this.connection.prepareStatement(
-					"SELECT COUNT(id) as 'quantidade', modelo, categoria FROM carros group by modelo, categoria;");
+					"SELECT COUNT(renavan) as 'quantidade', modelo, categoria FROM carros group by modelo, categoria;");
 
 			ResultSet rs = stmt.executeQuery();
 
@@ -462,4 +233,3 @@ public class CarrosDao {
 
 	}
 
-}
